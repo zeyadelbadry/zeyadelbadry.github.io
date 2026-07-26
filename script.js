25d82cc810927ba571c36ec540d94f8e34118652
zeyadelbadry <zezo9ua65@gmail.com>
2026-07-26 04:31:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -183,23 +183,9 @@ const counter = document.getElementById("imageCounter");
 let currentIndex = 0;
 
 function updateViewer(){
-
-    viewerImage.style.opacity = "0";
-    viewerImage.style.transform = "scale(.96)";
-
-    setTimeout(()=>{
-
-        viewerImage.src = images[currentIndex].src;
-
-        counter.innerHTML = ${currentIndex + 1} / ${images.length};
-
-        viewerImage.style.opacity = "1";
-        viewerImage.style.transform = "scale(1)";
-
-    },150);
-
+    viewerImage.src = images[currentIndex].src;
+    counter.innerHTML = ${currentIndex + 1} / ${images.length};
 }
-
 function openImage(src){
 
     currentIndex =
